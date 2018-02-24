0852c2abd609ae5f68607d9eb4f50c570d25b3fe
littleblackman <sandyrazafitrimo@gmail.com>
2018-02-24 14:42:47 +0100

    add the autoload

--- _config.php
+++ _config.php
@@ -3,18 +3,45 @@
 ini_set('display_errors','on');
 error_reporting(E_ALL);
 
-$root = $_SERVER['DOCUMENT_ROOT'];
-$host = $_SERVER['HTTP_HOST'];
 
-define('HOST', 'http://'.$host.'/devinette/');
-define('ROOT', $root.'devinette/');
+class MyAutoload
+{
+    public static function start()
+    {
+        
+        spl_autoload_register(array(__CLASS__, 'autoload'));
+        
+        $root = $_SERVER['DOCUMENT_ROOT'];
+        $host = $_SERVER['HTTP_HOST'];
 
-define('CONTROLLER', ROOT.'controller/');
-define('VIEW', ROOT.'view/');
-define('MODEL', ROOT.'model/');
-define('CLASSES', ROOT.'classes/');
+        define('HOST', 'http://'.$host.'/devinette/');
+        define('ROOT', $root.'devinette/');
+
+        define('CONTROLLER', ROOT.'controller/');
+        define('VIEW', ROOT.'view/');
+        define('MODEL', ROOT.'model/');
+        define('CLASSES', ROOT.'classes/');
 
 
-define('ASSETS', HOST.'assets/');
+        define('ASSETS', HOST.'assets/');
+    }
+
+    public static function autoload($class)
+    {
+        if(file_exists(MODEL.$class.'.php'))
+        {
+            include_once (MODEL.$class.'.php');
+        } else if (file_exists(CLASSES.$class.'.php'))
+        {
+            include_once (CLASSES.$class.'.php');
+        } else if (file_exists(CONTROLLER.$class.'.php'))
+        {
+            include_once (CONTROLLER.$class.'.php');
+        } ;
+
+    }
+}
+
+
 
 
--- classes/Routeur.php
+++ classes/Routeur.php
@@ -1,7 +1,5 @@
 <?php
 
-include_once(CONTROLLER.'Home.php');
-
 /**
  * Class Routeur
  *
